c9d91415c925cce46b2d4fa23582c16cdf952b82
nvwns1 <nvwns12345@gmail.com>
2024-05-29 19:16:01 +0545

    chore: Update ServiceCard component with image lazy loading and styling improvements

--- src/components/HomePage/ServiceSection/ServiceCard/ServiceCard.tsx
+++ src/components/HomePage/ServiceSection/ServiceCard/ServiceCard.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import styles from "./ServiceCard.module.scss";
+import Image from "next/image";
 
 export interface IServiceCardProps {
   id: number;
-  image?: string;
+  image: string;
   heading: string;
   description: string;
 }
@@ -11,7 +12,15 @@ export interface IServiceCardProps {
 const ServiceCard = (props: IServiceCardProps) => {
   return (
     <div className={styles.serviceCardRoot}>
-      <div className={styles.imageContainer}></div>
+      <div className={styles.imageContainer}>
+        <Image
+          className={styles.image}
+          width={200}
+          height={200}
+          src={props.image}
+          alt={props.heading}
+        />
+      </div>
       <div className={styles.heading}>{props.heading}</div>
       <div className={styles.description}>{props.description}</div>
     </div>
--- src/components/HomePage/ServiceSection/ServiceSection.tsx
+++ src/components/HomePage/ServiceSection/ServiceSection.tsx
@@ -10,21 +10,21 @@ const ServiceSection = () => {
       heading: "Catering",
       description:
         "We offer catering services for events and gatherings, providing an unforgettable taste of Nepal for your guests.",
-      image: "/images/catering.jpg",
+      image: "/homePage/services/catering.png",
     },
     {
       id: 2,
       heading: "Takeout",
       description:
         "Experience the rich flavors of Nepal on the go with our convenient takeout options.",
-      image: "/images/catering.jpg",
+      image: "/homePage/services/takeout.png",
     },
     {
       id: 3,
       heading: "Delivery",
       description:
         "Enjoy the convenience of having our delicious Nepali dishes delivered right to your doorstep.",
-      image: "/images/catering.jpg",
+      image: "/homePage/services/delivery.png",
     },
   ];
 

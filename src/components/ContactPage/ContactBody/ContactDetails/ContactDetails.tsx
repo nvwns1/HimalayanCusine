016b678efa8bc448499864ab7fe1d42bbbb4746d
nvwns1 <nvwns12345@gmail.com>
2024-06-02 16:33:46 +0545

    Add: Location Map

--- src/components/ContactPage/ContactBody/ContactDetails/ContactDetails.tsx
+++ src/components/ContactPage/ContactBody/ContactDetails/ContactDetails.tsx
@@ -15,8 +15,14 @@ const detailData: IDetailData[] = [
 const ContactDetails = () => {
   return (
     <div className={styles.contactDetailWrapper}>
-      {/*TODO: ADD MAP */}
-      <div className={styles.contactHeadingWrapper}></div>
+      <iframe
+        className={styles.contactHeadingWrapper}
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3513.5509170327473!2d-0.11363269512410477!3d51.486504897353065!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4876049284a69f57%3A0xd5112b823b377298!2sThe%20Oval!5e0!3m2!1sen!2snp!4v1717325087044!5m2!1sen!2snp"
+        width="400"
+        height="300"
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
       <div className={styles.contactBodyWrapper}>
         {detailData.map((data) => {
           return (

5f20411a191dda519799d4127356c21e3850db01
nvwns1 <nvwns12345@gmail.com>
2024-06-02 16:38:05 +0545

    chore: Update contact page with responsive Google Maps iframe

--- src/components/ContactPage/ContactBody/ContactDetails/ContactDetails.tsx
+++ src/components/ContactPage/ContactBody/ContactDetails/ContactDetails.tsx
@@ -17,9 +17,10 @@ const ContactDetails = () => {
     <div className={styles.contactDetailWrapper}>
       <iframe
         className={styles.contactHeadingWrapper}
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3513.5509170327473!2d-0.11363269512410477!3d51.486504897353065!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4876049284a69f57%3A0xd5112b823b377298!2sThe%20Oval!5e0!3m2!1sen!2snp!4v1717325087044!5m2!1sen!2snp"
-        width="400"
-        height="300"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d875.9867624869448!2d-0.2813755650613626!3d51.61103162879538!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487617cb003160e1%3A0x4108a34b44dd851!2sHimalayan%20Spices!5e0!3m2!1sen!2snp!4v1717325448931!5m2!1sen!2snp"
+        width="600"
+        height="450"
+        style={{ border: 0 }}
         loading="lazy"
         referrerPolicy="no-referrer-when-downgrade"
       ></iframe>

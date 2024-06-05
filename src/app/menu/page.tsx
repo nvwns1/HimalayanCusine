5c1d0b777a03c94fb052b93e05248d4a6d978aad
nvwns1 <nvwns12345@gmail.com>
2024-06-05 18:21:48 +0545

    chore: Add react-pdf dependency and implement menu page with PDF rendering

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,7 +1,25 @@
+import MenuPage from "@/components/MenuPage/MenuPage";
 import React from "react";
 
 const page = () => {
-  return <div>menu</div>;
+  return (
+    <div>
+      {/* <object
+        data="/pdf/menu.pdf"
+        type="application/pdf"
+        width="100%"
+        height="600px"
+      >
+        <param name="header" value="false" />
+        <p>
+          PDF cannot be displayed. You can download it{" "}
+          <a href="/path/to/your/pdf.pdf">here</a>.
+        </p>
+      </object> */}
+      {/* <embed src="/pdf/menu.pdf" width="100%" height="800px " /> */}
+      <MenuPage />
+    </div>
+  );
 };
 
 export default page;
---
+++ src/components/MenuPage/MenuPage.tsx
@@ -0,0 +1,58 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { Document, Page, pdfjs } from "react-pdf";
+import styles from "./MenuPage.module.scss";
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.mjs",
+  import.meta.url
+).toString();
+
+const MenuPage = () => {
+  const [numPages, setNumPages] = useState<number>();
+  const [pageNumber, setPageNumber] = useState<number>(0);
+  const [containerWidth, setContainerWidth] = useState<number>(0);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setContainerWidth(
+        document.getElementById("pdf-container")?.offsetWidth || 800
+      );
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
+    setNumPages(numPages);
+  }
+  return (
+    <section className={styles.menuSection}>
+      <div
+        id="pdf-container"
+        className="pdfContainer"
+        style={{ width: "100%", maxWidth: "1000px", margin: "0 auto" }}
+      >
+        <Document file="/pdf/menu.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+          {Array.apply(null, Array(numPages))
+            .map((x, i) => i + 1)
+            .map((page) => {
+              return (
+                <Page
+                  key={page}
+                  className={styles.pdfPage}
+                  pageNumber={page}
+                  renderTextLayer={false}
+                  renderAnnotationLayer={false}
+                  width={containerWidth || 800} // Set width dynamically
+                />
+              );
+            })}
+        </Document>
+      </div>
+    </section>
+  );
+};
+
+export default MenuPage;

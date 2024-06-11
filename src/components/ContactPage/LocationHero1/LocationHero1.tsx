aab2a9ceb4c4049f41c4e189f3eb537e0686ebfa
nvwns1 <nvwns12345@gmail.com>
2024-06-10 18:52:29 -0700

    refactor: minor changes of size of location Heading and gap

--- src/components/ContactPage/LocationHero1/LocationHero1.tsx
+++ src/components/ContactPage/LocationHero1/LocationHero1.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import styles from "./LocationHero1.module.scss";
-import ContactHeading from "../ContactHeading/ContactHeading";
-import ContactBody from "../ContactBody/ContactBody";
 import LocationBody from "../LocationBody/LocationBody";
 
 const ContactHero1 = () => {
